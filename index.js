a427c8601bb9c78385e49a31159976611b33427b
Jessica Lafferty <jessica.a.lafferty@gmail.com>
2024-09-12 10:34:32 -0400

    added

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const pg = require('pg')
+const app = express()
+const port = 2000
+const client = new pg.Client('postgres://localhost:5432/postgres')
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/api/flavorss', async (req, res) => {
+    const response = await client.query("SELECT * FROM flavors");
+    res.json(response.rows);
+})
+
+app.post('/api/flavors', async (req, res) => {
+    const { id, first_name, last_name } = req.body;
+    const response = await client.query("INSERT INTO flavors (id,name, is_favorite) VALUES ($1, $2, $3)", [id, name, is_favorite]);
+    res.json(`succesfully added flavor: ${name}`);
+})
+
+app.delete('/api/flavors/:id', async (req, res) => {
+    const { id } = req.params;
+    const response = await client.query("DELETE FROM flavors WHERE id = $1", [id]);
+    res.json(`succesfully deleted student: ${id}`);
+})
+
+app.listen(port, async () => {
+    await client.connect();
+    console.log(`Example app listening on port ${port}`)
+})
